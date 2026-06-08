c263f6dd7806e6cc8c414457bddd95b5a3c84876
Aswin Krishna <your_email@example.com>
2026-06-08 23:50:42 +0530

    updated navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,40 +1,56 @@
 import "./NavBar.css";
-import { useContext } from "react";
-import ThemeContext from "../../Themes/ThemeContext"
-import { MdOutlineLightMode } from "react-icons/md";
+import { useContext, useState } from "react";
+import ThemeContext from "../../Themes/ThemeContext";
+import { MdOutlineLightMode, MdMenu } from "react-icons/md";
 import { CiDark } from "react-icons/ci";
-
-
+import { IoClose } from "react-icons/io5";
 
 export default function Navbar() {
+  const { darkMode, handleTheme } = useContext(ThemeContext);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-  const {darkMode,handleTheme}=useContext(ThemeContext)
   return (
     <header className="navbar-wrapper">
-      <nav className="navbar" style={{color:darkMode?"black":"white"}}>
+      <nav
+        className="navbar"
+        style={{ color: darkMode ? "black" : "white" }}
+      >
         <a href="/" className="logo">
           Aswin Krishna
         </a>
 
-        <ul className="nav-links">
+        <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
           <li>
-            <a href="#work">Work</a>
+            <a href="#work" onClick={() => setMenuOpen(false)}>
+              Work
+            </a>
           </li>
 
           <li>
-            <a href="#lab">Lab</a>
+            <a href="#lab" onClick={() => setMenuOpen(false)}>
+              Lab
+            </a>
           </li>
 
           <li>
-            <a href="#connect">Connect</a>
+            <a href="#connect" onClick={() => setMenuOpen(false)}>
+              Connect
+            </a>
           </li>
         </ul>
 
-        <a onClick={handleTheme} className="nav-button">
-        {darkMode?<CiDark />
-:<MdOutlineLightMode />
-}
-        </a>
+        <div className="nav-actions">
+          <button onClick={handleTheme} className="nav-button">
+            {darkMode ? <CiDark /> : <MdOutlineLightMode />}
+          </button>
+
+          <button
+            className="menu-btn"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            {menuOpen ? <IoClose /> : <MdMenu />}
+          </button>
+        </div>
       </nav>
     </header>
   );
